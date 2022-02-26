0a11a6a9acbf2654f62caab54698acf392b4a6d4
HARDHILL-HOST\hardh <hardhill@mail.ru>
2022-02-26 22:26:55 +0800

    cards

--- app/src/main/java/ru/hardhill/comptest/MainActivity.kt
+++ app/src/main/java/ru/hardhill/comptest/MainActivity.kt
@@ -3,44 +3,70 @@ package ru.hardhill.comptest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.ExperimentalUnitApi
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import org.intellij.lang.annotations.JdkConstants
+
+
+import androidx.compose.ui.unit.*
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column() {
+                ListItem(name = "Waclaw", prof = "programmer")
+                ListItem(name = "Waclaw", prof = "programmer")
+                ListItem(name = "Waclaw", prof = "programmer")
+                ListItem(name = "Waclaw", prof = "programmer")
+                ListItem(name = "Waclaw", prof = "programmer")
+                ListItem(name = "Waclaw", prof = "programmer")
+            }
 
-            NewText()
         }
     }
 }
 
-@OptIn(ExperimentalUnitApi::class)
-@Preview(showBackground = true)
+
 @Composable
-fun NewText() {
-    Row(modifier = Modifier
-        .background(color = Color.LightGray).fillMaxSize(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically,
+fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 6.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp,
     ) {
-        Text(text = "Hello", fontSize = TextUnit(16f, TextUnitType.Sp))
-        Text(text = "Madrid", fontSize = TextUnit(16f, TextUnitType.Sp))
-        Text(text = "Girona", fontSize = TextUnit(16f, TextUnitType.Sp))
+        Box() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.book_cat),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(all = 10.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 10.dp)) {
+                    Text(text=name)
+                    Text(text=prof)
+                }
+            }
+        }
     }
-
 }
-
