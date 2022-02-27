988429e89e134454c41d4570afb952392c47c146
HARDHILL-HOST\hardh <hardhill@mail.ru>
2022-02-27 13:59:09 +0800

    cards

--- app/src/main/java/ru/hardhill/comptest/MainActivity.kt
+++ app/src/main/java/ru/hardhill/comptest/MainActivity.kt
@@ -1,13 +1,16 @@
 package ru.hardhill.comptest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +22,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 
 
 import androidx.compose.ui.unit.*
@@ -45,13 +49,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(name = "item")
+
 @Composable
 fun ListItem(name: String, prof: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 6.dp),
+            .padding(horizontal = 16.dp, vertical = 6.dp)
+            .clickable {
+                       Log.d("COMPOSE", "CLIck")
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp,
     ) {
